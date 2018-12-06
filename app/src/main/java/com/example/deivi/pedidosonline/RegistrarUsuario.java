c201cbdc948bff91a46e3b911e5e9c28180e502b
josuemendozapadilla <padillajosuemendoza8540940@gmail.com>
2018-12-06 01:49:24 -0400

    :

--- app/src/main/java/com/example/deivi/pedidosonline/RegistrarUsuario.java
+++ app/src/main/java/com/example/deivi/pedidosonline/RegistrarUsuario.java
@@ -1,14 +1,27 @@
 package com.example.deivi.pedidosonline;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.example.deivi.pedidosonline.utils.Data;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
 
 public class RegistrarUsuario extends AppCompatActivity {
     Button crearcuenta;
@@ -28,6 +41,15 @@ public class RegistrarUsuario extends AppCompatActivity {
         ArrayAdapter<CharSequence>adapter = ArrayAdapter.createFromResource(this,
                 R.array.tipousuario, android.R.layout.simple_spinner_item);
         tipo.setAdapter(adapter);
+
+        crearcuenta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                sedData();
+            }
+
+        });
 
 
 
@@ -67,4 +89,48 @@ public class RegistrarUsuario extends AppCompatActivity {
 
 
     }
+    public void sedData(){
+        TextView nombre  = findViewById(R.id.name1);
+        TextView ci  = findViewById(R.id.ci);
+        TextView telefono = findViewById(R.id.phone1);
+        TextView email  = findViewById(R.id.correo1);
+        TextView password  = findViewById(R.id.password1);
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.addHeader("authorization", Data.TOKEN);
+
+
+        RequestParams params = new RequestParams();
+
+        params.add("nombre", nombre.getText().toString());
+        params.add("ci", ci.getText().toString());
+        params.add("telefono",telefono.getText().toString());
+        params.add("email",email.getText().toString());
+        params.add("password",password.getText().toString());
+
+        client .post(Data.REGISTER_USER, params, new JsonHttpResponseHandler() {
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                //AsyncHttpClient.log.w(LOG_TAG, "onSuccess(int, Header[], JSONArray) was not overriden, but callback was received");
+                AlertDialog alertDialog = new AlertDialog.Builder(RegistrarUsuario.this).create();
+                try {
+                    String msn = response.getString("msn");
+                    alertDialog.setTitle("RESPONSE SERVER");
+                    alertDialog.setMessage(msn);
+                    alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+                    alertDialog.show();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+
+        });
+    }
+
 }

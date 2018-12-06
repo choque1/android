e222e95d5f0c66e4f9bd4078135836832fe6c4c5
josuemendozapadilla <padillajosuemendoza8540940@gmail.com>
2018-12-05 22:47:44 -0400

    ha

--- app/src/main/java/com/example/deivi/pedidosonline/InfoRestaurant.java
+++ app/src/main/java/com/example/deivi/pedidosonline/InfoRestaurant.java
@@ -1,13 +1,14 @@
 package com.example.deivi.pedidosonline;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class InfoRestaurant extends AppCompatActivity {
-    Button crear,ver,editar;
+    Button crear,ver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +16,17 @@ public class InfoRestaurant extends AppCompatActivity {
         setContentView(R.layout.activity_info_restaurant);
         crear = findViewById(R.id.crearmenu);
         ver = findViewById(R.id.vermenu);
-        editar = findViewById(R.id.editarmenu);
+        final TextView nombre=(TextView) findViewById(R.id.nombre);
+        nombre.setText( getIntent().getExtras().getString("nombre"));
+        final TextView calle=(TextView) findViewById(R.id.calle);
+        calle.setText( getIntent().getExtras().getString("calle"));
+        final TextView telefono=(TextView) findViewById(R.id.telefono);
+        telefono.setText( getIntent().getExtras().getString("telefono"));
+        final TextView propietario=(TextView) findViewById(R.id.property);
+        propietario.setText( getIntent().getExtras().getString("propietario"));
+
+        String _id_de_mi_restaurant=getIntent().getExtras().getString("_id");
+
         crear.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -27,13 +38,6 @@ public class InfoRestaurant extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(InfoRestaurant.this,VerMenu.class));
-                finish();
-            }
-        });
-        editar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(InfoRestaurant.this,EditarMenu.class));
                 finish();
             }
         });
